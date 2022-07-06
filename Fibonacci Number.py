96f596b9ee257674c26d2aa0f04173d904f90156
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-05 19:10:58 -0700

    Add files via upload

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -13,3 +13,16 @@ class Solution:
         if n == 1:
             return 1
         return self.fib(n-1) + self.fib(n-2)
+
+        # Solution 2
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        prev1, prev2 = 1,0
+        output = 0
+        for i in range(2, n + 1):
+            output = prev1 + prev2
+            prev2 = prev1
+            prev1 = output
+        return output
